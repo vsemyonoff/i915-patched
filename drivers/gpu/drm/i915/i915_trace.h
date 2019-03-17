2704c7dd734f394803d324fe065b081daa1dd585
Vladislav Semyonoff <vsemyonoff@gmail.com>
2019-03-17 14:49:39 +0200

    i915: fixed Dell XPS 9570 black screen issue

--- drivers/gpu/drm/i915/i915_trace.h
+++ drivers/gpu/drm/i915/i915_trace.h
@@ -977,5 +977,5 @@ DEFINE_EVENT(i915_context, i915_context_free,
 
 /* This part must be outside protection */
 #undef TRACE_INCLUDE_PATH
-#define TRACE_INCLUDE_PATH ../../drivers/gpu/drm/i915
+#define TRACE_INCLUDE_PATH .
 #include <trace/define_trace.h>
